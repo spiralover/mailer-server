d881899e0b4872f6346edf454b30f30e6d10de3c
ahmard <ahmard.mu@gmail.com>
2024-01-15 10:14:36 +0100

    fix(docker-helper): reverse variadic arch

--- docker-tag-and-push.sh
+++ docker-tag-and-push.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# Build Images: linux/amd64,linux/arm64
-printf "Building & Tagging %s -> v%s & v%s \n\n" "$1" "$2" "$3";
+# Build Images
+printf "Building & Tagging v%s & v%s \n\n" "$1" "$2";
 
 echo "-> building and push user service..."
-docker buildx build --platform "$1" \
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t spiralover/mailer-user-service:"$1" \
   -t spiralover/mailer-user-service:"$2" \
-  -t spiralover/mailer-user-service:"$3" \
   -t spiralover/mailer-user-service:latest . \
   -f apps/user/Dockerfile --push
 
 echo "-> building and push executor service..."
-docker buildx build --platform "$1" \
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t spiralover/mailer-executor-service:"$1" \
   -t spiralover/mailer-executor-service:"$2" \
-  -t spiralover/mailer-executor-service:"$3" \
   -t spiralover/mailer-executor-service:latest . \
   -f apps/executor/Dockerfile --push
